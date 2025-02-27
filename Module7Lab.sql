038ba2c92264ba6717bfa07b18a3812544d61990
Somi Seol <tl3222zx@go.minnstate.edu>
2025-02-27 17:52:09 +0000

    moldue 7 lab part 2

--- Module7Lab.sql
+++ Module7Lab.sql
@@ -44,3 +44,59 @@ PRINT 'Returned total amount: $' + CAST(ISNULL(@TotalAmount, 0) AS NVARCHAR(20))
 GO
 
 -- sqlcmd -S localhost -U sa -P P@ssw0rd -d Northwind -i Module7Lab.sql -o results.txt
+
+-- =============================================
+-- Part 2: CheckProductStock Procedure
+-- =============================================
+
+CREATE OR ALTER PROCEDURE CheckProductStock
+    @ProductID INT,
+    @NeedsReorder BIT OUTPUT -- (1) if need reordering (0) if not
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    DECLARE @UnitsInStock INT; 
+    DECLARE @ReorderLevel INT; 
+    DECLARE @ProductName NVARCHAR(40); 
+    
+    -- save product info
+    SELECT 
+        @UnitsInStock = UnitsInStock, 
+        @ReorderLevel = ReorderLevel, 
+        @ProductName = ProductName 
+    FROM Products 
+    WHERE ProductID = @ProductID;
+    
+    -- check if product exists
+    IF @ProductName IS NULL
+        BEGIN
+            SET @NeedsReorder = 0;
+            PRINT 'Product ID ' + CAST(@ProductID AS NVARCHAR(10)) + ' not found';
+            RETURN;
+        END
+    
+    -- check if reorder needed
+    IF @UnitsInStock <= @ReorderLevel
+        BEGIN
+            SET @NeedsReorder = 1;
+            PRINT @ProductName + ' needs reordering Stock: ' + 
+                CAST(@UnitsInStock AS NVARCHAR(10)) + 
+                ', ReorderLevel: ' + CAST(@ReorderLevel AS NVARCHAR(10));
+        END
+    ELSE
+        BEGIN
+            SET @NeedsReorder = 0;
+            PRINT @ProductName + ' has adequate stock Stock: ' + 
+                CAST(@UnitsInStock AS NVARCHAR(10)) + 
+                ', ReorderLevel: ' + CAST(@ReorderLevel AS NVARCHAR(10));
+        END
+END
+GO
+
+-- Test the new procedure
+DECLARE @NeedsReorder BIT;
+EXEC CheckProductStock 
+    @ProductID = 11,
+    @NeedsReorder = @NeedsReorder OUTPUT;
+PRINT 'Needs Reorder: ' + CAST(@NeedsReorder AS VARCHAR(1));
